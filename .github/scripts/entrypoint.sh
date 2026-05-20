1cb6e2124a78f9a6bb44d4f44772ec5831649b4b
kmn988 <98259756+kmn988@users.noreply.github.com>
2026-05-20 12:19:03 +1000

    update entrypoint.sh

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -3,9 +3,9 @@
 echo "Starting vowel frequency analyzer..."
 
 # Run the vowel frequency analyzer
-FREQ_RESULT=$(python /.github/scripts/frequency.py "/data.txt")
+FREQ_RESULT=$(python /app/.github/scripts/frequency.py /app/data.txt)
 
 #Running update_readme.sh
-bash /.github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
+bash /app/.github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
 
 echo "Process completed."
